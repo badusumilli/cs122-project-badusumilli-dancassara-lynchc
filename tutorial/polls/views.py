8e5063ebd4ecab52466706a0a613ea9de36e7635
Connor Lynch <lynchc@cs.uchicago.edu>
2016-02-26 10:17:04 -0600

    minor changes to views.py and models.py

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import HttpResponse
+from django import forms
+
+
+
+
 
 def index(request):
 	return HttpResponse ("Polls index")
@@ -7,15 +12,21 @@ def detail(request, quiz_id):
 	return HttpResponse("Quiz %s." % quiz_id)
 
 def results(request, quiz_id):
-	response = "Results of Question %s." 
+	response = "Results of Quiz %s." 
 	return HttpResponse(response %quiz_id)
 
 def vote(request, quiz_id):
-	return HttpResponse("Please answer Question %s." % quiz_id)
+	return HttpResponse("Complete the quiz below to determine your investment\
+     profile %s." % quiz_id)
 
 def quiz(request, quiz_id):
 	return HttpResponse("Quiz %s." % quiz_id)
 
-
+class Quizform(forms.Form):
+    q1 = forms.CharField(
+        label= "How old are you?"
+        help_text = 'e.g. 22'
+        required=True)
+    q2 = 
 
 # Create your views here.
