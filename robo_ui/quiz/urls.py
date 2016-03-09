867ee92b5d6953457a7d04c60d0e2f223476e4f2
Connor Lynch <lynchc@cs.uchicago.edu>
2016-03-09 13:22:53 -0600

    print added

--- robo_ui/quiz/urls.py
+++ robo_ui/quiz/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.quiz_form, name='quiz_form'),
-    url(r'^$', views.home, name='quiz_home'),   
+    url(r'^$', views.home, name='quiz_home'), 
+    url(r'^quiz_form', views.quiz_form, name='quiz_form'),
+    url(r'^results', views.results, name='results'),
+      
 ]
