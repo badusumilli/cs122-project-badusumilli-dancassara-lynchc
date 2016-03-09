a9b1bb72bf943501d866ff4fb1b6feb273ab5c32
Daniel Cassara <dancassara@cs.uchicago.edu>
2016-03-09 13:25:51 -0600

    pls work

--- data.py
+++ data.py
@@ -8,6 +8,7 @@ import pandas
 from pandas.io.data import DataReader
 import datetime
 import time
+import sys
 
 # ticker_list = ['VTI', 'VBR', 'VEA', 'VTMGX', 'VWO', 'VSS', 'VNQ', 'VNQI', 'BND', 'VBMFX', 'BSV', 'VBISX', 'BNDX', 'VGTSX']
 
@@ -24,7 +25,7 @@ import time
 
 
 
-def retrieve_hist_prices(ticker_list):
+def retrieve_hist_prices(ticker):
 	'''
 	Function heavily obtained from http://stackoverflow.com/questions/12433076/
 	download-history-stock-prices-automatically-from-yahoo-finance-in-python 
@@ -45,24 +46,26 @@ def retrieve_hist_prices(ticker_list):
 	connection = sqlite3.connect("roboadvisor.db")
 	c = connection.cursor()
 
-	for ticker in ticker_list:
+	# c.execute("DROP TABLE IF EXISTS " + ticker)
+	# df = DataReader(ticker,  'yahoo', datetime.datetime(1980,1,1), datetime.datetime.now())
 
-		c.execute("DROP TABLE IF EXISTS " + ticker)
-		df = DataReader(ticker,  'yahoo', datetime.datetime(1980,1,1), datetime.datetime.now())
+	# To get only most recent data
+	last_date = c.execute("SELECT date FROM " + ticker + " ORDER BY date DESC LIMIT 1")
+	last_date = last_date.fetchall()[0]
+	last_date = datetime.datetime.strptime(last_date[0], '%Y-%m-%d %H:%M:%S')
 
-		# To get data only from past week
-		# df = DataReader(ticker,  'yahoo', datetime.datetime.now() - datetime.timedelta(days = 7), datetime.now())
-		# Timestamp('2016-01-04 00:00:00')
+	if last_date.date() < datetime.datetime.now().date() - datetime.timedelta(days=1):
+		df = DataReader(ticker,  'yahoo', last_date + datetime.timedelta(days=1), datetime.datetime.now())
+	
 		dataframe = pandas.DataFrame(df)
 		dataframe["Date"] = dataframe.index
 		dataframe["Adj_Close"] = dataframe.pop("Adj Close")
 
-		# http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_sql.html
+	# http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_sql.html
 		dataframe.to_sql(ticker, connection, if_exists='append', index=False)
 
 	connection.commit()
 	connection.close
-
 
 
 # def drop_tables(ticker_list):
@@ -79,6 +82,32 @@ def retrieve_hist_prices(ticker_list):
 
 # 	connection.commit()
 # 	connection.close
+
+
+#######################################################################################333
+if __name__=="__main__":
+    num_args = len(sys.argv)
+
+    # if num_args != 2:
+    #     print("usage: python3 " + sys.argv[0] + " <file name for speaker A> " +
+    #           "<file name for speaker B>\n  <file name of text to identify> " +
+    #           "<order>")
+    #     sys.exit(0)
+
+    ticker = sys.argv[1]
+    retrieve_hist_prices(ticker)
+    
+    # for index in range(1, num_args):
+
+	   #  ticker = sys.argv[index]
+
+	   #  # # print(ticker_list)
+	   #  # print('arg 0: ', sys.argv[0])
+	   #  # print('arg 1: ', sys.argv[1])
+	   #  print(ticker)
+	   #  retrieve_hist_prices(ticker)
+
+
 
 
 
